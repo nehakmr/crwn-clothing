c95a3d3218ebdb0c58553556b444c172473e3b9e
nehakmr <neha2304shahi@gmail.com>
2021-06-17 15:34:38 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/> 
     </div>
   );
 }
